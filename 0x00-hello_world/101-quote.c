5d65ca1a269f8e3db893a9c15f61c904c6e00b06
Ayowills <rockaykingzy1@yahoo.com>
2023-06-19 14:38:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 
 /**
- * main - A c program that prints ecatly to the standard error
+ * main - A c program that prints to standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful
-	\" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
